82d08c4af2296d42126d6fc851584988c63d782c
Mariusz Rak <rak.m.rak@gmail.com>
2017-03-09 18:44:08 +0100

    changes in promises

--- server.js
+++ server.js
@@ -8,64 +8,61 @@ app.use(bodyParser.json());
 
 readFilePromise = filename => {
     return new Promise((res, rej) => {
-                fs.readFile(filename, 'utf8', (error, data) => {
-                    if (error) {
-                        rej(error);
-                    } else {
-                        res(data);
-                    }
-                });
+        fs.readFile(filename, 'utf8', (error, data) => {
+            if (error) {
+                const tempErr = new Error('Wystąpił błąd odczytu');
+                rej(tempErr);
+            } else {
+                res(data);
+            }
         });
+    });
 }
 
 writeFilePromise = (filename, data) => {
     return new Promise((res, rej) => {
-            fs.writeFile(filename, data, (error, data) => {
-                    if (error) {
-                        rej(error);
-                    } else {
-                        res();
-                    }
-                });
-        }
-    )
+        fs.writeFile(filename, data, (error, data) => {
+            if (error) {
+                const tempErr = new Error('Wystąpił błąd zapisu');
+                throw tempErr;
+                rej(tempErr);
+            } else {
+                res();
+            }
+        });
+    })
 }
 
-readErrorMsg = (error, res) => {
-    console.error('Wystąpił błąd podczas odczytu pliku test. ', error.message);
-    res.send('Bardzo nam przykro, wystąpiły kłopoty przy przetwarzaniu żądania');
+errorResponse = (error, res) => {
+    console.error(error.message);
+    res.send("Wystąpił błąd podczas przetwarzania pliku");
 }
 
-app.get('/getNote', function (req, res) {
-    console.log('Otrzymałem żądanie GET do getNote ' + res);
+app.get('/getNote', function(req, res) {
+    console.log('Otrzymałem żądanie GET do getNote');
 
     readFilePromise('./test.json')
-    .then(data => res.send('Zawartość pliku test.json: ' + data))
-    .catch(error => readErrorMsg(error, res))
-});
+        .then(data => res.send('Zawartość pliku test.json: ' + data))
+        .catch(error => errorResponse(error, res))
+})
 
+app.post('/updateNote/:note', function(req, res) {
 
-app.post('/updateNote/:note', function(req, res){
-    
     let responseTxt = '';
     let fileContent = '';
 
     console.log('Otrzymałem żądanie POST do updateNote');
 
     readFilePromise('./test.json')
-    .then (data => {
-        fileContent = data + req.params.note;
-        responseTxt = "Dotychczasowa zawartość pliku test.json: " + data + ' ... ';
-        writeFilePromise('./test.json', fileContent)
-        .then (() => {
+        .then(data => {
+            fileContent = data + req.params.note;
+            responseTxt = "Dotychczasowa zawartość pliku test.json: " + data + ' ... ';
+            return writeFilePromise('./test.json', fileContent);
+        })
+        .then(() => {
             responseTxt += "Nowa zawartość pliku test.json: " + fileContent;
             res.send(responseTxt);
         })
-        .catch (error => {
-            console.error('Wystąpił błąd podczas zapisu pliku test.json', error.message)
-            res.send('Bardzo nam przykro, wystąpiły kłopoty przy przetwarzaniu żądania');
-        })
-    })
-    .catch (error => readErrorMsg(error, res));
+        .catch(error => errorResponse(error, res));
 })
 app.listen(3000);
